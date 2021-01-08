8757b76f386e32b1b21d9fad0bf54f2d0213c6fd
Titus Kirch <requests@tkirch.dev>
2021-01-09 00:32:33 +0100

    feat: add languages field

--- files/lib/system/resolver/QueryResolver.class.php
+++ files/lib/system/resolver/QueryResolver.class.php
@@ -4,6 +4,7 @@ namespace graphql\system\resolver;
 use wcf\data\article\Article;
 use wcf\data\article\ArticleList;
 use wcf\data\language\Language;
+use wcf\data\language\LanguageList;
 use wcf\data\user\User;
 use wcf\data\user\UserList;
 
@@ -29,6 +30,14 @@ class QueryResolver extends AbstractResolver
             },
             'language' => function ($value, $args) {
                 return new Language($args['id']);
+            },
+            'languages' => function ($value, $args) {
+                $list = new LanguageList();
+                $list->sqlOffset = $args['skip'];
+                $list->sqlLimit = $args['first'];
+                $list->readObjects();
+
+                return $list->getObjects();
             },
             'user' => function ($value, $args) {
                 return new User($args['id']);

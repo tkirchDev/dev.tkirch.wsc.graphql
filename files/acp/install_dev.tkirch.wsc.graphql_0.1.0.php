6d0b866012b4488e95f1dc74e646fc8299993d17
Titus Kirch <requests@tkirch.dev>
2021-01-10 01:21:25 +0100

    feat: add credential tables

--- files/acp/install_dev.tkirch.wsc.graphql_0.1.0.php
+++ files/acp/install_dev.tkirch.wsc.graphql_0.1.0.php
@@ -1,11 +1,13 @@
 <?php
 use graphql\data\schema\SchemaAction;
+use wcf\system\database\table\column\DatetimeDatabaseTableColumn;
 use wcf\system\database\table\column\NotNullInt10DatabaseTableColumn;
 use wcf\system\database\table\column\NotNullVarchar255DatabaseTableColumn;
 use wcf\system\database\table\column\ObjectIdDatabaseTableColumn;
 use wcf\system\database\table\column\TextDatabaseTableColumn;
 use wcf\system\database\table\DatabaseTable;
 use wcf\system\database\table\DatabaseTableChangeProcessor;
+use wcf\system\database\table\index\DatabaseTableForeignKey;
 use wcf\system\WCF;
 
 $tables = [
@@ -16,6 +18,28 @@ $tables = [
             TextDatabaseTableColumn::create('filepath'),
             NotNullInt10DatabaseTableColumn::create('priority')
                 ->defaultValue(1000),
+        ]),
+    DatabaseTable::create('graphql1_credential')
+        ->columns([
+            ObjectIdDatabaseTableColumn::create('credentialID'),
+            NotNullVarchar255DatabaseTableColumn::create('name'),
+            NotNullVarchar255DatabaseTableColumn::create('key'),
+            TextDatabaseTableColumn::create('secret'),
+        ]),
+    DatabaseTable::create('graphql1_credential_token')
+        ->columns([
+            ObjectIdDatabaseTableColumn::create('credentialTokenID'),
+            NotNullInt10DatabaseTableColumn::create('credentialID'),
+            NotNullVarchar255DatabaseTableColumn::create('type'),
+            DatetimeDatabaseTableColumn::create('validUntil'),
+            DatetimeDatabaseTableColumn::create('createdAt'),
+        ])
+        ->foreignKeys([
+            DatabaseTableForeignKey::create()
+                ->columns(['credentialID'])
+                ->referencedTable('graphql1_credential')
+                ->referencedColumns(['credentialID'])
+                ->onDelete('CASCADE'),
         ]),
 ];
 

4ecff49952df48b3fdfa824d6a0ccbb206bdcd20
Titus Kirch <requests@tkirch.dev>
2021-01-10 22:17:32 +0100

    feat: add get credential function

--- files/lib/data/credential/token/CredentialToken.class.php
+++ files/lib/data/credential/token/CredentialToken.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace graphql\data\credential\token;
 
+use graphql\data\credential\Credential;
 use wcf\data\DatabaseObject;
 
 class CredentialToken extends DatabaseObject
@@ -9,4 +10,28 @@ class CredentialToken extends DatabaseObject
      * @inheritDoc
      */
     protected static $databaseTableIndexName = 'credentialTokenID';
+
+    /**
+     * store credential object
+     *
+     * @var Credential
+     */
+    protected $credential;
+
+    /**
+     * get credential from token
+     *
+     * @return Credential
+     */
+    public function getCredential(): Credential
+    {
+        if (empty($this->credential)) {
+            $credential = new Credential($this->credentialID);
+            if ($credential->credentialID) {
+                $this->credential = $credential;
+            }
+        }
+
+        return $this->credential;
+    }
 }

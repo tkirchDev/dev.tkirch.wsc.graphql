8be9f1ef5c7206a1ce5856ee25506df20a77155f
Titus Kirch <requests@tkirch.dev>
2021-01-12 20:42:46 +0100

    feat: comment all interfaces

--- files/lib/system/server/IServer.class.php
+++ files/lib/system/server/IServer.class.php
@@ -7,11 +7,49 @@ use GraphQL\Type\Schema;
 
 interface IServer
 {
-    public function __construct();
+    /**
+     * authentication function of the server
+     *
+     * @return void
+     */
     public function authenticate(): void;
+
+    /**
+     * build the schema of the server
+     *
+     * @return Schema
+     */
     public function buildSchema(): Schema;
+
+    /**
+     * execute the server
+     *
+     * @return void
+     */
     public function execute(): void;
+
+    /**
+     * register a resolver
+     *
+     * @param mixed $resolver
+     *
+     * @return void
+     */
     public function registerResolver($resolver): void;
+
+    /**
+     * register an array of resolvers
+     *
+     * @param array $resolvers
+     *
+     * @return void
+     */
     public function registerResolvers($resolvers = []): void;
+
+    /**
+     * set config for the server
+     *
+     * @return void
+     */
     public function setConfig(): void;
 }
